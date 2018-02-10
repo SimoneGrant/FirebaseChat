493a2b47bdbd68d14a81820681ecbefdc783c67f
SimoneGrant <simonegrant001@gmail.com>
2018-02-10 16:01:27 -0500

    Storyboard constraints

---
+++ FirebaseChat/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  FirebaseChat
+//
+//  Created by Simone Grant on 2/10/18.
+//  Copyright © 2018 Simone Grant. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    var sender: String = ""
+    var message: String = ""
+    var timeSent = Date()
+}
---
+++ FirebaseChat/View Controller/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  FirebaseChat
+//
+//  Created by Simone Grant on 2/10/18.
+//  Copyright © 2018 Simone Grant. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
