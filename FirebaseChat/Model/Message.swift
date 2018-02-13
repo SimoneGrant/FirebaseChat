3526596e538410bc777fed423c7aec87fcb55fe8
SimoneGrant <simonegrant001@gmail.com>
2018-02-12 23:44:04 -0500

    Send message to db

---
+++ FirebaseChat/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  FirebaseChat
+//
+//  Created by Simone Grant on 2/12/18.
+//  Copyright © 2018 Simone Grant. All rights reserved.
+//
+
+import Foundation
+
+class Message: NSObject {
+    var sender: String?
+    var message: String?
+}
--- FirebaseChat/View Controller/MessageLogViewController.swift
+++ FirebaseChat/View Controller/MessageLogViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import Firebase
 
 class MessageLogViewController: UIViewController {
     
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var chatCollectionView: UICollectionView!
     @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+    
+    // MARK: - Setup
+    
+    func setup() {
         navigationItem.title = "Chat Log"
+        messageTextField.delegate = self
         addSeparatorToView()
     }
     
@@ -24,17 +34,69 @@ class MessageLogViewController: UIViewController {
     func addSeparatorToView() {
         let separator = UIView()
         containerView.addSubview(separator)
-        separator.backgroundColor = UIColor.gray
+        separator.backgroundColor = UIColor.lightGray
         separator.translatesAutoresizingMaskIntoConstraints = false
         //constrants
         NSLayoutConstraint.activate ([
             separator.leftAnchor.constraint(equalTo: containerView.leftAnchor),
             separator.topAnchor.constraint(equalTo: containerView.topAnchor),
             separator.widthAnchor.constraint(equalTo: containerView.widthAnchor),
-            separator.heightAnchor.constraint(equalToConstant: 0.5)
+            separator.heightAnchor.constraint(equalToConstant: 0.7)
             ])
     }
     
+    // MARK: - Action
     
+    @IBAction func sendButtonPressed(_ sender: UIButton) {
+        messageTextField.endEditing(true)
+        messageTextField.isEnabled = false
+        sendButton.isEnabled = false
+        let messageRef = Database.database().reference().child("messages")
+        let childRef = messageRef.childByAutoId()
+        let values = ["Sender": Auth.auth().currentUser?.email, "MessageBody": messageTextField.text!, "Name": ""]
+        childRef.setValue(values) {
+            (error, reference) in
+            if error != nil {
+                print("Could not update message database", error!)
+                return
+            }
+            print("Message saved!")
+            self.messageTextField.isEnabled = true
+            self.sendButton.isEnabled = true
+            self.messageTextField.text = ""
+        }
+    }
     
+    func retrieveMessageFromDatabase() {
+        let messageRef = Database.database().reference().child("Messages")
+        messageRef.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! [String: String]
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            
+        }
+    }
+    
+}
+
+extension MessageLogViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        sendButtonPressed(sendButton)
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
 }
